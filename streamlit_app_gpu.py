f31f846fd8fc1477c154bea08a952b5d565b7393
KarimIbrahim11 <karim.ibrahim.amin@gmail.com>
2023-05-21 02:36:26 +0300

    Added GPU inference file for locall hosting, modified CPU inference for deployment

--- streamlit_app_gpu.py
+++ streamlit_app_gpu.py
@@ -1,13 +1,10 @@
 '''
 
-This webui is designed to showcase the results in a user-friendly way. However, it uses CPU
-as streamlit servers do not provide GPUs in its free service. Therefore, the inference here
-is way slower. For GPU inference please clone the repo, install the packages ( you can find
-it in requirements.txt) and 'pip install streamlit'. You can then run the streamlit webui on
-your machine by 'streamlit run streamlit_app_gpu.py'. My machine's nividia computability ra-
-ting is 6.1 and it runs lightning fast.
+This streamlit service can only run locally on your machine as it support GPU inference.
+'streamlit run streamlit_app_gpu.py' will do the job :)
 
 '''
+
 import torch
 import os
 import torch.nn.functional as F
@@ -19,7 +16,6 @@ from io import BytesIO
 import numpy as np
 
 
-device = 'cpu'
 # Loading the model Once for Optimization
 @st.cache_resource
 def loadModel():
@@ -29,12 +25,12 @@ def loadModel():
                   'heads': [1, 2, 4, 8], 'ffn_expansion_factor': 2.66, 'bias': False, 'LayerNorm_type': 'WithBias',
                   'dual_pixel_task': False}
     weights = os.path.join('Motion_Deblurring', 'pretrained_models', 'motion_deblurring.pth')
-    # Load Model Arch and convert load to CPU
+    # Load Model Arch and convert to CUDA
     load_arch = run_path(os.path.join('basicsr', 'models', 'archs', 'restormer_arch.py'))
     model = load_arch['Restormer'](**parameters)
-    model.cpu()
+    model.cuda()
     # Load weights&Params to Model
-    checkpoint = torch.load(weights, map_location=device)
+    checkpoint = torch.load(weights)
     model.load_state_dict(checkpoint['params'])
     print("************** Model Loaded **************")
     return model, weights
@@ -84,12 +80,16 @@ if __name__ == '__main__':
         img_multiple_of = 8
         print(f"\n ==> Running {task} with weights {weights}\n ")
         with torch.no_grad():
-            # Load image in cpu
+            torch.cuda.ipc_collect()
+            torch.cuda.empty_cache()
+
             img = Image.open(uploaded_file).convert('RGB')
             img = np.array(img)
-            input_ = torch.from_numpy(img).float().div(255.).permute(2, 0, 1).unsqueeze(0).cpu()
+            # # Convert RGB to BGR
+            # img = open_cv_image[:, :, ::-1].copy()
+            # img = cv2.cvtColor(cv2.imread("img." + pilImg.format), cv2.COLOR_BGR2RGB)
+            input_ = torch.from_numpy(img).float().div(255.).permute(2, 0, 1).unsqueeze(0).cuda()
 
-            # Pre-Processing
             # Pad the input if not_multiple_of 8
             h, w = input_.shape[2], input_.shape[3]
             H, W = ((h + img_multiple_of) // img_multiple_of) * img_multiple_of, (
@@ -98,24 +98,25 @@ if __name__ == '__main__':
             padw = W - w if w % img_multiple_of != 0 else 0
             input_ = F.pad(input_, (0, padw, 0, padh), 'reflect')
 
-            # Infere image
             restored = model(input_)
             restored = torch.clamp(restored, 0, 1)
 
-            # Post Processing
             # Unpad the output
             restored = restored[:, :, :h, :w]
             restored = restored.permute(0, 2, 3, 1).cpu().detach().numpy()
             restored = img_as_ubyte(restored[0])
 
-            # Image Sharpening
+            # # Post Processing
+            # # Image Sharpening
             sharpen_filter = [
                 0, -1, 0,
                 -1, 5, -1,
                 0, -1, 0
             ]
             im = Image.fromarray(restored.astype('uint8'), 'RGB')
+            # Create a kernel filter using the defined matrix
             kernel_filter = ImageFilter.Kernel((3, 3), sharpen_filter)
+            # Apply the kernel filter to the image
             restored = im.filter(kernel_filter)
 
             # Show the user that we have finished
